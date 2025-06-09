5145e373ccf9d40ab3ca222a1aa975191c31955d
sangoonthego <knkidngoc@gmail.com>
2025-06-09 16:44:26 +0700

    Update ExampleRepository.java

--- src/main/java/com/jpdictionary/demo/repository/ExampleRepository.java
+++ src/main/java/com/jpdictionary/demo/repository/ExampleRepository.java
@@ -4,7 +4,9 @@ import com.jpdictionary.demo.models.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ExampleRepository extends JpaRepository<Example, Long> {
-	
+    List<Example> findByWord_Id(Long wordId);  // Note the underscore here for nested property
 }
