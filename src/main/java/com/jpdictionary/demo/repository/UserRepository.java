db6ae6c60433ba26553579ad2099a1685f576ce3
sangoonthego <knkidngoc@gmail.com>
2025-06-09 16:45:47 +0700

    Update UserRepository.java

--- src/main/java/com/jpdictionary/demo/repository/UserRepository.java
+++ src/main/java/com/jpdictionary/demo/repository/UserRepository.java
@@ -2,11 +2,19 @@ package com.jpdictionary.demo.repository;
 
 import com.jpdictionary.demo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(value = "SELECT TOP 1 * FROM users WHERE username = :username", nativeQuery = true)
     Optional<User> findByUsername(String username);
+
+    @Query(value = "SELECT TOP 1 * FROM users WHERE email = :email", nativeQuery = true)
     Optional<User> findByEmail(String email);
+
+    @Query(value = "SELECT * FROM users WHERE id = :id", nativeQuery = true)
+    Optional<User> findUserById(Long id);
 }
