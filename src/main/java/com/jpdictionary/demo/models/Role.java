0841e89a8f7e3887a545665b2e3aaa607b6bd5dc
sangoonthego <knkidngoc@gmail.com>
2025-06-09 16:43:12 +0700

    Update Role.java

--- src/main/java/com/jpdictionary/demo/models/Role.java
+++ src/main/java/com/jpdictionary/demo/models/Role.java
@@ -7,25 +7,37 @@ import lombok.Data;
 @Entity
 @Table(name = "roles")
 public class Role {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    @Column(name = "role_name", nullable = false, unique = true)
+
+    @Column(name = "role_name", nullable = false)
     private String name;
     
     public Role() {
-    	
+		// Default constructor
 	}
     
     public Role(Long id, String name) {
+		this.id = id;
 		this.name = name;
 	}
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
