73882ac466b055b295a11e856dfb8ca33b8b22f7
sangoonthego <knkidngoc@gmail.com>
2025-06-09 16:45:13 +0700

    Update RoleRepository.java

--- src/main/java/com/jpdictionary/demo/repository/RoleRepository.java
+++ src/main/java/com/jpdictionary/demo/repository/RoleRepository.java
@@ -3,9 +3,8 @@ package com.jpdictionary.demo.repository;
 import com.jpdictionary.demo.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String name);  // Tìm role theo tên
+    Role findByName(String name);  // Tìm role theo tên
 }
