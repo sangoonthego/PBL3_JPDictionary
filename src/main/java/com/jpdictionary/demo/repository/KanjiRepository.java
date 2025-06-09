bacdb568ab1c67db28f87d1ecc1219342fb7b54b
sangoonthego <knkidngoc@gmail.com>
2025-06-09 16:44:40 +0700

    Update KanjiRepository.java

--- src/main/java/com/jpdictionary/demo/repository/KanjiRepository.java
+++ src/main/java/com/jpdictionary/demo/repository/KanjiRepository.java
@@ -2,9 +2,15 @@ package com.jpdictionary.demo.repository;
 
 import com.jpdictionary.demo.models.Kanji;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 @Repository
 public interface KanjiRepository extends JpaRepository<Kanji, Long> {
-	
+
+    @Query(value = "SELECT * FROM kanji WHERE jlpt_level = :level", nativeQuery = true)
+    List<Kanji> findKanjiByJlptLevel(@Param("level") int level);
 }
