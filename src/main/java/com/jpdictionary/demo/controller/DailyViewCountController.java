9b762ea7422cb9326f6f36a7868539e533990b2a
sangoonthego <knkidngoc@gmail.com>
2025-06-09 16:33:12 +0700

    Update DailyViewCountController.java

--- src/main/java/com/jpdictionary/demo/controller/DailyViewCountController.java
+++ src/main/java/com/jpdictionary/demo/controller/DailyViewCountController.java
@@ -3,6 +3,8 @@ package com.jpdictionary.demo.controller;
 import com.jpdictionary.demo.models.DailyViewCount;
 import com.jpdictionary.demo.service.DailyViewCountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,17 +17,31 @@ public class DailyViewCountController {
     private DailyViewCountService dailyViewCountService;
 
     @PostMapping("/increment")
-    public DailyViewCount incrementDailyView() {
-        return dailyViewCountService.incrementTodayViewCount();
+    public ResponseEntity<DailyViewCount> incrementDailyView() {
+        DailyViewCount updatedCount = dailyViewCountService.incrementTodayViewCount();
+        if (updatedCount != null) {
+            return ResponseEntity.ok(updatedCount);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @GetMapping("/today")
-    public DailyViewCount getTodayCount() {
-        return dailyViewCountService.getTodayViewCount();
+    public ResponseEntity<DailyViewCount> getTodayCount() {
+        DailyViewCount todayCount = dailyViewCountService.getTodayViewCount();
+        if (todayCount != null) {
+            return ResponseEntity.ok(todayCount);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/all")
-    public List<DailyViewCount> getAllDailyViewCounts() {
-        return dailyViewCountService.getAllViewCounts();
+    public ResponseEntity<List<DailyViewCount>> getAllDailyViewCounts() {
+        List<DailyViewCount> allCounts = dailyViewCountService.getAllViewCounts();
+        if (allCounts.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(allCounts);
     }
 }
