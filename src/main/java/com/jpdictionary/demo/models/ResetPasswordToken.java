1c7b676b3923b089f6f2ad7a07359184fd678410
sangoonthego <knkidngoc@gmail.com>
2025-06-09 14:46:57 +0700

    upload ResetPasswordToken

---
+++ src/main/java/com/jpdictionary/demo/models/ResetPasswordToken.java
@@ -0,0 +1,89 @@
+package com.jpdictionary.demo.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "reset_password_tokens")
+public class ResetPasswordToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime expiryTime;
+
+    private boolean used = false;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public ResetPasswordToken() {
+		// Default constructor
+	}
+    
+    public ResetPasswordToken(String token, User user, LocalDateTime expiryTime) {
+		this.token = token;
+		this.user = user;
+		this.expiryTime = expiryTime;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public LocalDateTime getExpiryTime() {
+		return expiryTime;
+	}
+
+	public void setExpiryTime(LocalDateTime expiryTime) {
+		this.expiryTime = expiryTime;
+	}
+
+	public boolean isUsed() {
+		return used;
+	}
+
+	public void setUsed(boolean used) {
+		this.used = used;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+}
+
